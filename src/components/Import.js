d42875a4b5e1e10b7fad5343ab85a09814f8ab0e
CyrellEstrada <cyrellestrada1@gmail.com>
2025-01-03 17:16:31 -0600

    Not Completed

--- src/components/Import.js
+++ src/components/Import.js
@@ -1,10 +1,64 @@
 import React from 'react'
+import { Button, Table, TableHead, TableBody, TableRow, TableCell, Container, TableContainer, Paper } from '@mui/material'
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
+import { MoreVert } from '@mui/icons-material'
+import { deleteMake } from '../redux/actions';
 
 const Import = (props) => {
-    // fill out this component
-
+    console.log(props)
+    const [anchorEl, setAnchorEl] = React.useState(null);
+    const open = Boolean(anchorEl);
+    const handleClick = (event) => {
+        setAnchorEl(event.currentTarget);
+    };
+    const handleClose = () => {
+        setAnchorEl(null);
+    };
     return (
-        <p>Import Component</p>
+        <>
+        <Button variant="contained" onClick={props.fetchMakes}>Import</Button>
+        <TableContainer component={Paper}>
+            <Table sx={{ minWidth: 650 }} aria-label="simple table">
+                <TableHead>
+                    <TableRow>
+                        <TableCell>Id</TableCell>
+                        <TableCell>Makes</TableCell>
+                        <TableCell>Actions</TableCell>
+                    </TableRow>
+                </TableHead>
+                <TableBody>
+                    {props.makes.map((make, idx) => {
+                        return (
+                        <TableRow key={idx}>
+                            <TableCell>{make.MakeId}</TableCell>
+                            <TableCell>{make.MakeName}</TableCell>
+                            <TableCell>{make.VehicleTypeName}</TableCell>
+                            <MoreVert
+                            sx={{ cursor: "pointer" }} id="basic-button"
+                            aria-controls={open ? 'basic-menu' : undefined}
+                            aria-haspopup="true"
+                            aria-expanded={open ? 'true' : undefined}
+                            onClick={handleClick}>
+                            </MoreVert>
+                            <Menu
+                            id="basic-menu"
+                            anchorEl={anchorEl}
+                            open={open}
+                            onClose={handleClose}
+                            MenuListProps={{
+                            'aria-labelledby': 'basic-button',
+                            }}
+                        >
+                            <MenuItem onClick={deleteMake}>Delete</MenuItem>
+                        </Menu>
+                        </TableRow>
+                        )
+                    })}
+                </TableBody>
+            </Table>
+        </TableContainer>
+        </>
     )
 }
 
--- src/redux/actions.js
+++ src/redux/actions.js
@@ -13,3 +13,27 @@ export const removeCar = (index) => {
         value: index
     }
 }
+
+export const fetchMakes = () => {
+    return (dispatch) => {
+        console.log("test")
+        fetch("https://vpic.nhtsa.dot.gov/api/vehicles/GetMakesForVehicleType/car?format=json")
+            .then(res => res.json())
+            .then(response => {
+                console.log(response.Results)
+                const action = {
+                    type: 'FETCH_MAKES',
+                    value: response.Results
+                }
+                console.log(action)
+                dispatch(action)
+            })
+    }
+}
+
+export const deleteMake = (index) => {
+    return {
+        type: 'DELETE_MAKES',
+        value: index
+    }
+}
